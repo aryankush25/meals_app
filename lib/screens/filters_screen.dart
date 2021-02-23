1b5f5632fcae0c354996534204364a4a0144bc2d
Aryan Agarwal <aryankush025@gmail.com>
2021-02-23 15:55:48 +0530

    drawer added

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Filters',
+          ),
+        ),
+        // drawer: MainDrawer(),
+        body: Container(
+          child: Text(
+            'Filters Screen',
+          ),
+        ),
+      ),
+    );
+  }
+}
